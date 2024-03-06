64105d2fc2ffb3f542fef5b4326b3e54be8202cf
Durgesh <durgeshdalvi87@gmail.com>
2024-03-06 15:43:02 +0530

    used a separate script for products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,6 @@
-const products = [ {
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: ' Blackand Gray Athletic Cotton Socks - 6 Pairs',
-  rating: 
-  {
-    stars: 4.5,
-    count: 87 
-  },
-  priceCents: 1090  // js have problem doing math with floats thats why we will not use decimal point in the numeric value. (10 dollar 90 cents)
-},
-  
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: ' Intermediate Size Basketball',
-  rating:
-  {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: 
-  {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}
-];
 
 let productsHTML = '';
-
+// products variable will be coming from data/products.js
 products.forEach((product) => {
   productsHTML += `
        <div class="product-container">
